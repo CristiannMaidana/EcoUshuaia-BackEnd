a5dc4b02cb5466aadae4036edab3ed1542833e56
Cristian <cristian.mg.david@gmail.com>
2025-08-17 14:42:04 -0300

    feat(apiApp/urls): agregar nueva url-m- Agregar url para calendarios

--- ApiEcoUshuaia/apiApp/urls.py
+++ ApiEcoUshuaia/apiApp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from apiApp.views.calendarios_views import CalendariosViewSet
 from apiApp.views.residuos_views import ResiduosViewSet
 
 router = DefaultRouter()
 router.register('residuos', ResiduosViewSet, basename='residuos')
+router.register('calendarios', CalendariosViewSet, basename='calendarios')
 urlpatterns = [
     path('', include(router.urls)),
 ]
