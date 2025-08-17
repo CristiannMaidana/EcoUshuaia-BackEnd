cf848692025c2ab3edebb6874985a218ffb88e6a
Cristian <cristian.mg.david@gmail.com>
2025-08-17 15:24:17 -0300

    feat(apiApp/urls): agregar nueva url-m- Agregar url para coordendas

--- ApiEcoUshuaia/apiApp/urls.py
+++ ApiEcoUshuaia/apiApp/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from apiApp.views.calendarios_views import CalendariosViewSet
+from apiApp.views.coordenada_views import CoordendaViewSet
 from apiApp.views.residuos_views import ResiduosViewSet
 
 router = DefaultRouter()
 router.register('residuos', ResiduosViewSet, basename='residuos')
 router.register('calendarios', CalendariosViewSet, basename='calendarios')
+router.register('coordenadas', CoordendaViewSet, basename='coordenadas')
 urlpatterns = [
     path('', include(router.urls)),
 ]
