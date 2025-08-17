bfa3834a0c66893fcff5b2607e99e46f994c8871
Cristian <cristian.mg.david@gmail.com>
2025-08-17 14:41:08 -0300

    feat(calendarios_views): crear clase calendarios_views-m- Crear controlador con filtros

---
+++ ApiEcoUshuaia/apiApp/views/calendarios_views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, permissions
+from rest_framework import filters
+from apiApp.models import Calendarios
+from apiApp.serializers.calendarios_serializer import CalendarioSerializer
+
+
+class CalendariosViewSet(viewsets.ModelViewSet):
+    queryset = Calendarios.objects.all()
+    serializer_class = CalendarioSerializer
+
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('fecha',)
+    ordering_fields = ('id_calendario', 'fecha', 'hora')
