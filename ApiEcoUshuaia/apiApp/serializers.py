7d87d20a19378e2c3d7f9b396fbdca0621869ec9
Cristian <cristian.mg.david@gmail.com>
2025-08-17 13:21:35 -0300

    feat(serializers): crear clase serializer-m- Crear el serializer de Residuos-m- Crear validate_peso

---
+++ ApiEcoUshuaia/apiApp/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Residuos
+
+
+class ResiduosSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Residuos
+        fields = '__all__'
+
+    def validate_peso(self, value):
+        if value is not None and value < 0:
+            raise serializers.ValidationError("El peso no puede ser negativo.")
+        return value
