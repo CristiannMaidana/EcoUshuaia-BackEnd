d4a7bebb22dd01daea8756bcded76dae246d808b
Cristian <cristian.mg.david@gmail.com>
2025-08-18 13:49:31 -0300

    feat(direcciones_views): agregar la clase DireccionSerializer

--- ApiEcoUshuaia/apiApp/views/direcciones_views.py
+++ ApiEcoUshuaia/apiApp/views/direcciones_views.py
@@ -1,11 +1,12 @@
 from rest_framework import viewsets, filters
 
 from apiApp.models import Direcciones
+from apiApp.serializers.direcciones_serializer import DireccionSerializer
 
 
 class DireccionesViewSet(viewsets.ModelViewSet):
     queryset = Direcciones.objects.all()
-    serializer_class = Direcciones
+    serializer_class = DireccionSerializer
 
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('calle',)
