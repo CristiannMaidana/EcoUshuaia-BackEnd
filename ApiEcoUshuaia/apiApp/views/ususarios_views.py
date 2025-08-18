7185e734762ec127bde3d753a2631cce34029376
Cristian <cristian.mg.david@gmail.com>
2025-08-18 12:31:29 -0300

    feat(usuarios_views): crear nueva clase-m- Crear clase UsuariosViewSet-m- Crear filtros de busqueda

---
+++ ApiEcoUshuaia/apiApp/views/ususarios_views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, filters
+
+from apiApp.models import Usuarios
+from apiApp.serializers.ususarios_serializer import UsuariosSerializer
+
+
+class UsuariosViewSet(viewsets.ModelViewSet):
+    queryset = Usuarios.objects.select_related('id_zona', 'id_tipo_usuario').all()
+    serializer_class = UsuariosSerializer
+
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('email', 'id_zona__nombre_zona', 'id_tipo_ususario__tipo_usuario')
+    ordering_fields = ('id_usuario', 'nombre_usuario', 'apellido_usuario', 'id_zona__nombre_zona')
