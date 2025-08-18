0691a328010115deb855481a8b2d11f42ea64205
Cristian <cristian.mg.david@gmail.com>
2025-08-18 12:32:48 -0300

    feat(apiApp/urls): crear nueva url-m- Importar archivo UsuariosViews-m- Agregar nueva url a la ruta

--- ApiEcoUshuaia/apiApp/urls.py
+++ ApiEcoUshuaia/apiApp/urls.py
@@ -5,12 +5,14 @@ from apiApp.views.calendarios_views import CalendariosViewSet
 from apiApp.views.coordenada_views import CoordendaViewSet
 from apiApp.views.notificaciones_views import NotificacionesViewSet
 from apiApp.views.residuos_views import ResiduosViewSet
+from apiApp.views.ususarios_views import UsuariosViewSet
 
 router = DefaultRouter()
 router.register('residuos', ResiduosViewSet, basename='residuos')
 router.register('calendarios', CalendariosViewSet, basename='calendarios')
 router.register('coordenadas', CoordendaViewSet, basename='coordenadas')
 router.register('notificaciones', NotificacionesViewSet, basename='notificaciones')
+router.register('usuarios', UsuariosViewSet, basename='usuarios')
 urlpatterns = [
     path('', include(router.urls)),
 ]
