e5c8b174b3a2af52881c287577532dc0a64e5bc4
Cristian <cristian.mg.david@gmail.com>
2025-08-17 21:03:45 -0300

    feat(apiApp/urls): crear url-m- Crear url para notificaciones

--- ApiEcoUshuaia/apiApp/urls.py
+++ ApiEcoUshuaia/apiApp/urls.py
@@ -3,12 +3,14 @@ from rest_framework.routers import DefaultRouter
 
 from apiApp.views.calendarios_views import CalendariosViewSet
 from apiApp.views.coordenada_views import CoordendaViewSet
+from apiApp.views.notificaciones_views import NotificacionesViewSet
 from apiApp.views.residuos_views import ResiduosViewSet
 
 router = DefaultRouter()
 router.register('residuos', ResiduosViewSet, basename='residuos')
 router.register('calendarios', CalendariosViewSet, basename='calendarios')
 router.register('coordenadas', CoordendaViewSet, basename='coordenadas')
+router.register('notificaciones', NotificacionesViewSet, basename='notificaciones')
 urlpatterns = [
     path('', include(router.urls)),
 ]
