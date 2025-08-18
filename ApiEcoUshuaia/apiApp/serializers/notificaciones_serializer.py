eddf5e95825591bfbf10d0f34cc501c9e8f28ff7
Cristian <cristian.mg.david@gmail.com>
2025-08-17 21:01:57 -0300

    feat(notificaciones_serializer): refactorizar metodo-m- Refactorizar validate_fecha_envio para no ser cargado con una fecha anterior

--- ApiEcoUshuaia/apiApp/serializers/notificaciones_serializer.py
+++ ApiEcoUshuaia/apiApp/serializers/notificaciones_serializer.py
@@ -1,6 +1,5 @@
-from datetime import date
+from django.utils import timezone
 from rest_framework import serializers
-
 from apiApp.models import Notificaciones
 
 
@@ -10,8 +9,13 @@ class NotificacionesSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id_notificacion',)
 
-    def validate_fecha_envio(self, fecha_envio):
-        today = date.today()
-        if fecha_envio < today:
+    def validate_fecha_envio(self, value):
+        if timezone.is_naive(value):
+            value = timezone.make_aware(value, timezone.get_current_timezone())
+
+        fecha_value = timezone.localtime(value).date()
+        today = timezone.localdate()
+
+        if fecha_value < today:
             raise serializers.ValidationError('La fecha no puede ser anterior a hoy.')
-        return fecha_envio
+        return value
