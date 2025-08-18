3f678223b45910b20ebaa49d6ea9242a66300097
Cristian <cristian.mg.david@gmail.com>
2025-08-18 13:02:02 -0300

    feat(zonas_views): crear nuevo archivo-m- Crear nueva clase ZonasViews-m- Crear filtros

---
+++ ApiEcoUshuaia/apiApp/views/zonas_views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, filters
+
+from apiApp.models import Zonas
+from apiApp.serializers.zonas_serializer import ZonasSerializer
+
+
+class ZonasViewSet(viewsets.ModelViewSet):
+    queryset = Zonas.objects.all()
+    serializer_class = ZonasSerializer
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('nombre_zona',)
+    ordering_fields = ('id_zona', 'nombre_zona')
