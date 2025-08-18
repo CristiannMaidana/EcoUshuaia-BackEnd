3ff60cf9b825410e964bd60860427ffb9ed79975
Cristian <cristian.mg.david@gmail.com>
2025-08-17 21:14:17 -0300

    feat(usuarios_serializer): crear nueva clase-m- Crear clase serializer de usuarios

---
+++ ApiEcoUshuaia/apiApp/serializers/ususarios_serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from apiApp.models import Usuarios
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuarios
+        fields = '__all__'
+        read_only_field =  ('id_usuario',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Usuarios.objects.all(),
+                fields=('email',),
+                message= 'Este usuario ya existe en la base de datos'
+            )
+        ]
+            
