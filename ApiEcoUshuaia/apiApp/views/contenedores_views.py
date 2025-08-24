ead3f9504545df4449e1d2c8f5f917f79450829f
Cristian <cristian.mg.david@gmail.com>
2025-08-24 17:45:56 -0300

    feat(contenedores_views): agregar todas las tablas relacionadas, agregar filterbackend-m- En 'queryset' agregar los ids de las tablas relacionadas-m- agregar 'DjangoFilterBackend'

--- ApiEcoUshuaia/apiApp/views/contenedores_views.py
+++ ApiEcoUshuaia/apiApp/views/contenedores_views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import viewsets
 
@@ -6,9 +7,14 @@ from apiApp.serializers.contenedores_serializer import ContenedoresSerializer
 
 
 class ContenedoresViewSet(viewsets.ModelViewSet):
-    queryset = Contenedores.objects.select_related('id_zona', 'id_residuo',).all()
+    queryset = Contenedores.objects.select_related('id_zona', 'id_residuo', 'id_coordenada', 'id_mapa',).all()
     serializer_class = ContenedoresSerializer
 
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend)
     search_fields = ('nombre_contenedor', 'id_residuo__nombre')
     ordering_fields = ('nombre_contenedor', 'id_residuo__categoria','id_zona__nombre_zona')
+    filter_fields = {
+        'id_residuo': ['exact'],
+        'id_residuo__nombre': ['exact'],
+        'id_residuo__categoria': ['exact'],
+    }
