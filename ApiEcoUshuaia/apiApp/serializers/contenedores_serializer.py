c990a739df825b976a0217c70c49b24850e45274
Cristian <cristian.mg.david@gmail.com>
2025-08-24 17:43:21 -0300

    feat(contenedores_serializer): crear nuevo serializer y modificar propiedad id_residuo-m- Crear 'ResiduoLiteSerializer', agregar propiedades a mostrar de la tabla 'Residuos'-m- En 'ContenedoresSerializer' hacer solo de lectura 'id_residuo', agregar nuevo archivo de 'ResiduoLiteSerializer'

--- ApiEcoUshuaia/apiApp/serializers/contenedores_serializer.py
+++ ApiEcoUshuaia/apiApp/serializers/contenedores_serializer.py
@@ -1,11 +1,27 @@
-from apiApp.models import Contenedores
+from apiApp.models import Contenedores, Residuos
 from rest_framework import serializers
 
 
+class ResiduoLiteSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='id_residuo')
+    class Meta:
+        model = Residuos
+        fields = ('id', 'nombre','categoria')
+
+
 class ContenedoresSerializer(serializers.ModelSerializer):
+    id_residuo = serializers.PrimaryKeyRelatedField(
+        queryset=Residuos.objects.all(),
+        write_only=True
+    )
+
+    residuo = ResiduoLiteSerializer(source='id_residuo', read_only=True)
+
     class Meta:
         model = Contenedores
-        fields = '__all__'
+        fields = ('id_contenedor', 'nombre_contenedor', 'color_contenedor', 'capacidad_total', 'fecha_instalacion',
+                  'ultimo_vaciado', 'descripcion_ubicacion', 'id_zona', 'id_residuo', 'id_coordenada', 'id_mapa',
+                  'residuo')
         read_only_field = ('id_contenedor',)
 
     def validate_capacidad_total(self, value):
