6c19807e43390c50eed79194e83cd9a05f77e5cd
Cristian <cristian.mg.david@gmail.com>
2025-08-24 13:51:08 -0300

    feat(contenedores_serializer): crear validate-m- Crear 'validate_capacidad_total' para valores menores a 0

--- ApiEcoUshuaia/apiApp/serializers/contenedores_serializer.py
+++ ApiEcoUshuaia/apiApp/serializers/contenedores_serializer.py
@@ -7,4 +7,8 @@ class ContenedoresSerializer(serializers.ModelSerializer):
         model = Contenedores
         fields = '__all__'
         read_only_field = ('id_contenedor',)
-        
+
+    def validate_capacidad_total(self, value):
+        if value is not None and  value <= 0:
+            raise serializers.ValidationError('La capacidad total debe ser mayor a 0.')
+        return value
