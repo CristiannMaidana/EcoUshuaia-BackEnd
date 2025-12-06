04feb9e695052a8151888857fc9fc21ffa60a016
Cristian <cristian.mg.david@gmail.com>
2025-12-06 20:21:50 -0300

    feat(apiApp/urls): borrar router-m- Borrar importarcion y router de modelo coordenadas

--- ApiEcoUshuaia/apiApp/urls.py
+++ ApiEcoUshuaia/apiApp/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from apiApp.views.calendarios_views import CalendariosViewSet
 from apiApp.views.categoria_noticias_views import CategoriaNoticiasViewSet
 from apiApp.views.contenedores_views import ContenedoresViewSet
-from apiApp.views.coordenada_views import CoordendaViewSet
 from apiApp.views.direcciones_views import DireccionesViewSet
 from apiApp.views.notificaciones_views import NotificacionesViewSet
 from apiApp.views.residuos_views import ResiduosViewSet
@@ -15,7 +14,6 @@ from apiApp.views.zonas_views import ZonasViewSet
 router = DefaultRouter()
 router.register('residuos', ResiduosViewSet, basename='residuos')
 router.register('calendarios', CalendariosViewSet, basename='calendarios')
-router.register('coordenadas', CoordendaViewSet, basename='coordenadas')
 router.register('notificaciones', NotificacionesViewSet, basename='notificaciones')
 router.register('usuarios', UsuariosViewSet, basename='usuarios')
 router.register('contenedores', ContenedoresViewSet, basename='contenedores')
