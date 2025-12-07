b6f57fa35a12113602bb7daff213fafd538a9b88
Cristian <cristian.mg.david@gmail.com>
2025-12-06 21:22:18 -0300

    feat(contenedores_views): importar InBBoxFilter-m- Agregar filtro de busqueda-m- Crear filtro coordendad

--- ApiEcoUshuaia/apiApp/views/contenedores_views.py
+++ ApiEcoUshuaia/apiApp/views/contenedores_views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework import viewsets
+from rest_framework_gis.filters import InBBoxFilter
 
 from apiApp.models import Contenedores
 from apiApp.serializers.contenedores_serializer import ContenedoresSerializer
@@ -10,7 +11,7 @@ class ContenedoresViewSet(viewsets.ModelViewSet):
     queryset = Contenedores.objects.select_related('id_zona', 'id_residuo', 'id_mapa',).all()
     serializer_class = ContenedoresSerializer
 
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend)
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend, InBBoxFilter, )
     search_fields = ('nombre_contenedor', 'id_residuo__nombre')
     ordering_fields = ('nombre_contenedor', 'id_residuo__categoria','id_zona__nombre_zona')
     filter_fields = {
@@ -18,3 +19,4 @@ class ContenedoresViewSet(viewsets.ModelViewSet):
         'id_residuo__nombre': ['exact'],
         'id_residuo__categoria': ['exact'],
     }
+    bbox_filterset_fields = {'coordenada'}
