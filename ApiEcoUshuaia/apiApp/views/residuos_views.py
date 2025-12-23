4733ca48f13477354e2da90fb61ff5020660c5bd
Cristian <cristian.mg.david@gmail.com>
2025-12-23 11:12:50 -0300

    feat(residuos_views): cambiar queryset
    - Cambiar queryset para entregar los residuos ordenados por id

--- ApiEcoUshuaia/apiApp/views/residuos_views.py
+++ ApiEcoUshuaia/apiApp/views/residuos_views.py
@@ -6,7 +6,8 @@ from apiApp.serializers.residuos_serializer import ResiduosSerializer
 
 
 class ResiduosViewSet(ModelViewSet):
-    queryset = Residuos.objects.all()
+    queryset = (Residuos.objects.all().order_by('id_residuo'))
+    serializer_class = ResiduosSerializer
     serializer_class = ResiduosSerializer
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
