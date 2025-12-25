bc8ce99ed846e3b8fc03076f6d516f0749f9efbb
Cristian <cristian.mg.david@gmail.com>
2025-12-25 14:57:57 -0300

    refactor(residuos_views): borrar codigo repetido
    - Eliminar serializer_class que esta duplicada la linea de codigo

--- ApiEcoUshuaia/apiApp/views/residuos_views.py
+++ ApiEcoUshuaia/apiApp/views/residuos_views.py
@@ -8,7 +8,6 @@ from apiApp.serializers.residuos_serializer import ResiduosSerializer
 class ResiduosViewSet(ModelViewSet):
     queryset = (Residuos.objects.all().order_by('id_residuo'))
     serializer_class = ResiduosSerializer
-    serializer_class = ResiduosSerializer
 
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['id_residuo', 'categoria', 'nombre']
