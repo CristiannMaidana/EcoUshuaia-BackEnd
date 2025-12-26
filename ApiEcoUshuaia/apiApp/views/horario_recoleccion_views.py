fc6084b26b48acc0ac1650224c38d20655569e86
Cristian <cristian.mg.david@gmail.com>
2025-12-26 20:30:09 -0300

    feat(horario_recoleccion_view): crear metodo
    - Crear metodo 'horario_dia_zona', para devolver una lista filtrada segun los parametros ingresados en la url

--- ApiEcoUshuaia/apiApp/views/horario_recoleccion_views.py
+++ ApiEcoUshuaia/apiApp/views/horario_recoleccion_views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apiApp.models import HorariosRecoleccion
 from apiApp.serializers.horario_recoleccion_serializer import HorarioRecoleccionSerializer
@@ -10,3 +12,11 @@ class HorarioRecoleccionViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.OrderingFilter, filters.SearchFilter)
     search_fields = ('id_horario_recoleccion', 'dia_semana' 'hora_inicio')
+
+    # GET /api/horarios-recoleccion/horario_inicio=06:00:00/dia_semana=1/zona_id=1
+    @action(detail=False, methods=['get'], url_path=r'horario_inicio/(?P<hhmmss>\d{2}:\d{2}:\d{2})/dia_semana/(?P<dia>[0-6])/zona_id/(?P<zona>[0-6])')
+    def horario_dia_zona(self, request, hhmmss, dia, zona):
+        vals = (HorariosRecoleccion.objects
+                .filter(id_zona=int(zona), dia_semana=int(dia), hora_inicio=hhmmss)
+                .values_list('id_categoria_residuo', flat=True))
+        return Response(list(vals))
