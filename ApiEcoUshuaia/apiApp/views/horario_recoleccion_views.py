f0e7451773acfd679dc9d9449b89e0ea33d74a04
Cristian <cristian.mg.david@gmail.com>
2025-12-26 20:32:00 -0300

    feat(horario_recoleccion_views): crear metodo
    - Crear metodo de_dia, para obtener una lista filtrada por dia seleccionado en la url

--- ApiEcoUshuaia/apiApp/views/horario_recoleccion_views.py
+++ ApiEcoUshuaia/apiApp/views/horario_recoleccion_views.py
@@ -13,10 +13,19 @@ class HorarioRecoleccionViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.OrderingFilter, filters.SearchFilter)
     search_fields = ('id_horario_recoleccion', 'dia_semana' 'hora_inicio')
 
-    # GET /api/horarios-recoleccion/horario_inicio=06:00:00/dia_semana=1/zona_id=1
+    # GET /api/horarios-recoleccion/horario_inicio/06:00:00/dia_semana/1/zona_id/1
     @action(detail=False, methods=['get'], url_path=r'horario_inicio/(?P<hhmmss>\d{2}:\d{2}:\d{2})/dia_semana/(?P<dia>[0-6])/zona_id/(?P<zona>[0-6])')
     def horario_dia_zona(self, request, hhmmss, dia, zona):
         vals = (HorariosRecoleccion.objects
                 .filter(id_zona=int(zona), dia_semana=int(dia), hora_inicio=hhmmss)
                 .values_list('id_categoria_residuo', flat=True))
         return Response(list(vals))
+
+    # GET /api/horarios-recoleccion/dia/1/
+    @action(detail=False, methods=['get'], url_path=r'de-dia/(?P<dia>[0-6])')
+    def de_dia(self, request, dia):
+        rows = (HorariosRecoleccion.objects
+                .filter(dia_semana=int(dia))
+                .values('hora_inicio', 'id_categoria_residuo')
+                .order_by('hora_inicio'))
+        return Response(list(rows))
