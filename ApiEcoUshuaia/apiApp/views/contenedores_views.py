d6585f2cadb9d3cb25512c57b039f3ead7dfccee
Cristian <cristian.mg.david@gmail.com>
2025-12-31 12:47:22 -0300

    feat(contenedores_views): crear metodo
    - Crear metodo 'filtros_residuos', para obtener la lista de contenedores en base a los ids de residuos
    
    - Obtener los parametros de la url en ids, comprobar si es uno o muchos y separar las comas, convertirlos a una lista de tipo int
    
    - Hacer la query con la lista de ids, para dejar los que fueron seleccionados
    
    - Serializar la query resultante par que tenga la misma representacion en json que los contenedores solos

--- ApiEcoUshuaia/apiApp/views/contenedores_views.py
+++ ApiEcoUshuaia/apiApp/views/contenedores_views.py
@@ -1,5 +1,7 @@
 from rest_framework import filters
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework_gis.filters import InBBoxFilter
 
 from apiApp.models import Contenedores
@@ -19,3 +21,24 @@ class ContenedoresViewSet(viewsets.ModelViewSet):
         'id_zona':    ['exact'],
         'id_mapa':    ['exact'],
     }
+
+
+    # GET /api/contenedores/filtros/?residuos=1,3,5   (o ?residuos=1&residuos=3&residuos=5)
+    @action(detail=False, methods=['get'], url_path=r'filtros')
+    def filtros_residuos(self, request):
+        # Obtenemos ids de tipo String desde url
+        ids = request.query_params.getlist('residuos')
+        if len(ids) == 1 and ',' in ids[0]:
+            ids = [x.strip() for x in ids[0].split(',') if x.strip()]
+        try:
+            #Convertimos ids en numero, si no nada
+            ids = [int(x) for x in ids]
+        except (TypeError, ValueError):
+            return Response([])
+
+        # Filtramos los contenedores donde coincidan todos los ids
+        qs = self.get_queryset().filter(id_residuo_id__in=ids) if ids else self.get_queryset().none()
+
+        #Serializar el resultado de la query
+        ser = self.get_serializer(qs, many=True)
+        return Response(ser.data)
