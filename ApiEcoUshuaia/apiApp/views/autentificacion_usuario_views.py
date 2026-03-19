3a3f3ad8cdd973d08d349907748c6cca6696ba45
Cristian <cristian.mg.david@gmail.com>
2026-03-19 16:40:21 -0300

    feat(autentificacion_usuario_views): crear nuevo viewset
    - Verificar user obtenido en post, entregar todos los datos del usuario

---
+++ ApiEcoUshuaia/apiApp/views/autentificacion_usuario_views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from apiApp.serializers.autentificacion_usuario_serializer import AutentificacionUsuarioSerializer
+from apiApp.serializers.usuarios_serializer import UsuariosSerializer
+
+
+class AutentificacionUsuarioViewSet(viewsets.GenericViewSet):
+    serializer_class = AutentificacionUsuarioSerializer
+
+    @action(detail=False, methods=['post'], url_path='login')
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        perfil = serializer.validated_data["perfil"]
+
+        data = UsuariosSerializer(perfil).data
+        return Response(data, status=200)
