8edfe9c4fdce3424e7a95cbf9945ddf85943e79d
Cristian <cristian.mg.david@gmail.com>
2026-03-23 17:21:04 -0300

    feat(usuarios_views): agregar metodo para obtener al usuario

--- ApiEcoUshuaia/apiApp/views/usuarios_views.py
+++ ApiEcoUshuaia/apiApp/views/usuarios_views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, filters
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from apiApp.models import Usuarios
 from apiApp.serializers.usuarios_serializer import UsuariosSerializer
@@ -13,3 +15,9 @@ class UsuariosViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('id_usuario', 'email', )
     ordering_fields = ('id_usuario', 'nombre_usuario', 'apellido_usuario', 'id_zona__nombre_zona')
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated], url_path='me')
+    def me(self, request):
+        usuario = Usuarios.objects.select_related('id_zona', 'id_tipo_usuario').get(user=request.user)
+        serializer = self.get_serializer(usuario)
+        return Response(serializer.data)
