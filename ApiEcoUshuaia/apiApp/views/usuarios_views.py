faa91318c010b89afbf46406a493e5338df91869
Cristian <cristian.mg.david@gmail.com>
2026-04-01 18:32:13 -0300

    feat(usuarios_views): cambiar permiso de acceso y agregar id
    - Cambiar permiso para que todos puedan crearse una cuenta
    
    - Agregar propiedad id_direccion

--- ApiEcoUshuaia/apiApp/views/usuarios_views.py
+++ ApiEcoUshuaia/apiApp/views/usuarios_views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from apiApp.models import Usuarios
@@ -8,13 +8,19 @@ from apiApp.serializers.usuarios_serializer import UsuariosSerializer
 
 
 class UsuariosViewSet(viewsets.ModelViewSet):
-    queryset = Usuarios.objects.select_related('id_zona', 'id_tipo_usuario', 'user').all()
+    queryset = Usuarios.objects.select_related('id_direccion', 'id_zona', 'id_tipo_usuario', 'user').all()
     serializer_class = UsuariosSerializer
     permission_classes = [IsAuthenticated]
 
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
     search_fields = ('id_usuario', 'email', )
     ordering_fields = ('id_usuario', 'nombre_usuario', 'apellido_usuario', 'id_zona__nombre_zona')
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+
+        return [permission() for permission in self.permission_classes]
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -26,7 +32,7 @@ class UsuariosViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get', 'patch'], permission_classes=[IsAuthenticated], url_path='me')
     def me(self, request):
-        usuario = self.get_queryset().select_related('id_zona', 'id_tipo_usuario').get(user=request.user)
+        usuario = self.get_queryset().select_related('id_direccion', 'id_zona', 'id_tipo_usuario').get(user=request.user)
 
         if request.method == 'GET':
             serializer = self.get_serializer(usuario)
